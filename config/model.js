a8d84c7d12043b9a9033916f869b72294bb557b5
Felipe Verri <pipe.verri@gmail.com>
2025-04-16 11:36:43 -0300

    Modelo de Libro y Conexion a la DB terminados

---
+++ config/model.js
@@ -0,0 +1,44 @@
+const { Sequelize } = require('sequelize');
+
+const Database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite' // or any path you want for the DB file
+});
+
+
+const Libro = Database.define('Libro', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    titulo: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    autor: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    isbn: {
+      type: DataTypes.STRING(13),
+      unique: true,
+      allowNull: false
+    },
+    categoria: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    estado: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    fecha_creacion: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    timestamps: false // Desactivar los timestamps automáticos
+  });
+
+module.exports = Libro;
